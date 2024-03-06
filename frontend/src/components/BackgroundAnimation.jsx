f854395efe79e0122f1f55a7dfb179563869ed6e
omar tarik yehya <khalaf.omar20@gmail.com>
2024-03-06 18:48:15 +0800

    feat: background animation

---
+++ frontend/src/components/BackgroundAnimation.jsx
@@ -0,0 +1,29 @@
+
+const BackgroundAnimation = () => {
+    return (
+      <div className="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      </div>
+    )
+  }
+  
+  export default BackgroundAnimation
---
+++ frontend/src/components/Overlay.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Overlay = () => {
+  return (
+    <div className="glassmorphism-container">
+      <div className="glassmorphism-effect">
+      <div class="wrap">
+      <button class="button">Submit</button>
+      </div>
+      </div>
+    </div>  );
+}
+
+export default Overlay;
