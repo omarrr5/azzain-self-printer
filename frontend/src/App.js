51c80562f3e846837d6048fc861ff35f8150b12c
omar tarik yehya <khalaf.omar20@gmail.com>
2024-03-07 11:13:02 +0800

    feat: created routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './views/Home';
+import Order from './views/Order';
+import NotFound from './views/NotFound';
+import Login from './views/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+      <Routes>
+       <Route path="/" element={<Login/>}/>
+       <Route path="/home" element={<Home/>}/>
+       <Route path="/order" element={<Order/>}/>
+       <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </Router>
     </div>
   );
 }
