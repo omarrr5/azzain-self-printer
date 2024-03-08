3a5a7b939ae20cdb82c7979c259570b8b38670c1
omar tarik yehya <khalaf.omar20@gmail.com>
2024-03-08 11:52:32 +0800

    feat: created protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './index.css';
 import {RouterProvider} from "react-router-dom";
 import router from './router.jsx';
+import { ContextProvider } from './contexts/ContextProvider.jsx';
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <ContextProvider>
+        <RouterProvider router={router} />
+      </ContextProvider>
     </div>
   );
 }
--- frontend/src/components/DeafultLayout.jsx
+++ frontend/src/components/DeafultLayout.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from '../contexts/ContextProvider';
 
 const DeafultLayout = () => {
+
+  const {user, token} = useStateContext();
+
+  if(!token){
+    return <Navigate to="/login" />
+  }
+
   return (
-    <div>
-      deafult layout
+    <div> 
       <Outlet/>
     </div>
   )
