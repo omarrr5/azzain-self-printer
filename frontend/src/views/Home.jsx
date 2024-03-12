6d7707be8b8d796f6c18e5df46d87ccc218d05d3
omar tarik yehya <khalaf.omar20@gmail.com>
2024-03-12 11:29:23 +0800

    feat: Generate unique QR code

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/logo.png';
 import BackgroundAnimation from '../components/BackgroundAnimation';
 import QRCode from 'react-qr-code';
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
+  const [qrCodeValue, setQRCodeValue] = useState('');
+
+  useEffect(() => {
+    setQRCodeValue(generateQRCodeValue());
+  }, []); 
 
   const handleButtonClick = () => {
     setShowModal(true);
@@ -13,6 +18,12 @@ const Home = () => {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
+  function generateQRCodeValue() {
+    return `https://example.com/${Date.now()}`;
+  }
+
+
 
   return (
     <div className="glassmorphism-container">
@@ -30,7 +41,7 @@ const Home = () => {
             <div className="modal">
               <div className="modal-content">
                 <span className="close" onClick={handleCloseModal}>&times;</span>
-                <QRCode value="https://example.com" bgColor="#f5f5f5" fgColor="#00b4ff" />
+                <QRCode value={qrCodeValue} bgColor="#f5f5f5" fgColor="#00b4ff" />
                 <h2>Scan The QR Code To Upload Your Documents</h2>
               </div>
             </div>
